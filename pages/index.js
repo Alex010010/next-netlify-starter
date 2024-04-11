06efc70d935f0fa24476aa23c89b9c1eed1e7c3f
Alex010010 <alex.010.010@gmail.com>
2024-04-11 18:23:31 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import { useState } from 'react';
+import TaskForm from '@components/TaskForm';
 
 export default function Home() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="container">
       <Head>
@@ -10,14 +24,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header title="Welcome to my app! Alex010" />
+
       <main>
-        <Header title="Welcome to my app! Alex010" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1>Todo App</h1>
+        <TaskForm onAddTask={handleAddTask} />
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => handleDeleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
